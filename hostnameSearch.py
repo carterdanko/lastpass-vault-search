1bca1714263a8cdcbe783ebb0da9c5c65c43a0d2
Carter Danko <carter.danko@gmail.com>
2016-07-27 22:18:34 -0400

    redo for lastpass-cli 1.0.0

--- hostnameSearch.py
+++ hostnameSearch.py
@@ -6,7 +6,6 @@ from collections import defaultdict
 from fuzzywuzzy import process
 from workflow.workflow import Workflow
 
-# vaultMap = dict()
 vaultMap = defaultdict(list)
 UPDATE_INTERVAL = 3600 * 3
 
@@ -33,24 +32,24 @@ def gethostIds(wf):
     else:
         hostnameLookup(query)
 
-
 def updateIdCache():
 
-    vaultRaw = subprocess.check_output('/usr/local/bin/lpass ls', shell=True)
+    vaultRaw = subprocess.check_output('/usr/local/bin/lpass ls -l', shell=True)
 
-    for row in vaultRaw.split('/'):
+    for row in vaultRaw.split('\n'):
         if row != '(none)':
             try:
-                hostname=row.split(' [id: ')[0]
-                hostId=re.sub('[^0-9]', '', row.split(' [id: ')[1])
-                addToLocal(hostname, hostId)
+                hostname=row.split('/')[1].split(' [id:')[0]
+                hostId=row.split(' [id: ')[1].split('] ')[0]
+                username=row.split(' [username: ')[1].split(']')[0]
+                addToLocal(hostname, hostId, username)
             except:
                 None
 
     wf.cache_data('hostIdList', vaultMap)
 
-def addToLocal(hostname, hostId):
-    vaultMap[hostname].append(hostId)
+def addToLocal(hostname, hostId, username):
+    vaultMap[hostname].append([hostId, username])
 
 def hostnameLookup(hostname):
     if not wf.cached_data_fresh('hostIdList', max_age=UPDATE_INTERVAL) or wf.cached_data('hostIdList') == None:
@@ -60,24 +59,19 @@ def hostnameLookup(hostname):
     # spits back the top five results, this will have to be relayed to the workflow and then user selected
     # once it does we have the key for the map and we can get the id.
 
-    #topChoice = some user choice always going to be results[X][0] for the index in the list of tuples and then the str and not the length
-    for iterHost in results:
-        for hostId in localCache.get(iterHost[0]):
-            lpShow = '/usr/local/bin/lpass show {}'.format(hostId)
-            rawInfo = subprocess.check_output(lpShow, shell=True)
-            username = 'NO USERNAME'
-            url = ''
-            if rawInfo.__contains__('Username') and rawInfo.__contains__('URL'):
-                username = rawInfo.split('\n')[1].split('Username:')[1].strip()
-                url = rawInfo.split('\n')[3].split('URL:')[1].strip()
-            wf.add_item(title=iterHost[0],
-                        subtitle=username+' '+url+' \u2318-Click to copy username; ',
-                         modifier_subtitles={
-                            'cmd': '\u2318-Click to copy username.'
-                        },
-                        arg=hostId,
-                        icon='icon.png',
-                        valid=True)
+    for host in results:
+        info=localCache.get(host[0])
+        hostId=info[0][0]
+        username=info[0][1]
+        wf.add_item(title=host[0],
+                    subtitle=username + ' ' + ' \u2318-Click to copy username; ',
+                    modifier_subtitles={
+                        'cmd': '\u2318-Click to copy username.'
+                    },
+                    arg=hostId,
+                    icon='icon.png',
+                    valid=True)
+
     sendFeedback()
 
 def sendFeedback():
