6166cb12f0a6c90ba1b754f7865e65a490d77285
Brad Hurley <bradley.hurley@gmail.com>
2016-08-16 22:20:05 -0400

    making the project more pythonic

--- getPassword.py
+++ getPassword.py
@@ -2,25 +2,26 @@ import subprocess
 import sys
 from workflow import Workflow
 
-def getPassword(wf):
-    if len(wf.args):
-        vaultId = wf.args[0]
-    else:
-        vaultId = None
 
-    processedInfo = dict()
-    lpShow = '/usr/local/bin/lpass show {}'.format(vaultId)
-    rawInfo = subprocess.check_output(lpShow, shell=True)
-    for row in rawInfo.split('\n'):
+def get_password(wf):
+    if wf.args:
+        vault_id = wf.args[0]
+    else:
+        vault_id = None
+
+    processed_info = dict()
+    lp_show = '/usr/local/bin/lpass show {}'.format(vault_id)
+    raw_info = subprocess.check_output(lp_show, shell=True)
+    for row in raw_info.split():
         try:
             if row.split(":")[0] == 'Username' or row.split(":")[0] == 'Password':
-                processedInfo[row.split(":")[0]] = row.split(":")[1]
+                processed_info[row.split(":")[0]] = row.split(":")[1]
         except:
             None
 
     process = subprocess.Popen('pbcopy', env={'LANG': 'en_US.UTF-8'}, stdin=subprocess.PIPE)
-    process.communicate(processedInfo.get('Password').encode('utf-8').strip())
+    process.communicate(processed_info.get('Password').encode('utf-8').strip())
 
-if __name__ == u"__main__":
+if __name__ == "__main__":
     wf = Workflow()
-    sys.exit(wf.run(getPassword))
+    sys.exit(wf.run(get_password))
--- hostnameSearch.py
+++ hostnameSearch.py
@@ -6,55 +6,58 @@ from fuzzywuzzy import process
 from workflow.workflow import Workflow
 
 # vaultMap = dict()
-vaultMap = defaultdict(list)
+vault_map = defaultdict(list)
 
-def gethostIds(wf):
 
-    if len(wf.args):
+def get_host_ids(wf):
+
+    if wf.args:
         query = wf.args[0]
     else:
         query = None
 
-    vaultRaw = subprocess.check_output('/usr/local/bin/lpass ls', shell=True)
+    vault_raw = subprocess.check_output('/usr/local/bin/lpass ls', shell=True)
 
-    for row in vaultRaw.split('/'):
+    for row in vault_raw.split('/'):
         if row != '(none)':
             try:
-                hostname=row.split(' [id: ')[0]
-                hostId=re.sub('[^0-9]', '', row.split(' [id: ')[1])
-                addToLocal(hostname, hostId)
+                hostname = row.split(' [id: ')[0]
+                host_id = re.sub('[^0-9]', '', row.split(' [id: ')[1])
+                add_to_local(hostname, host_id)
             except:
                 None
 
-    hostnameLookup(query)
+    hostname_lookup(query)
 
 
-def addToLocal(hostname, hostId):
-    vaultMap[hostname].append(hostId)
+def add_to_local(hostname, host_id):
+    vault_map[hostname].append(host_id)
 
-def hostnameLookup(hostname):
-    results = process.extract(hostname, vaultMap.keys(), limit=5)
+
+def hostname_lookup(hostname):
+    results = process.extract(hostname, vault_map.keys(), limit=5)
     # spits back the top five results, this will have to be relayed to the workflow and then user selected
     # once it does we have the key for the map and we can get the id.
 
-    #topChoice = some user choice always going to be results[X][0] for the index in the list of tuples and then the str and not the length
-    for iterHost in results:
-        for hostId in vaultMap.get(iterHost[0]):
-            lpShow = '/usr/local/bin/lpass show {}'.format(hostId)
-            rawInfo = subprocess.check_output(lpShow, shell=True)
+    # topChoice = some user choice always going to be results[X][0] for the index in the list of
+    # tuples and then the str and not the length
+    for iter_host in results:
+        for host_id in vault_map.get(iter_host[0]):
+            lp_show = '/usr/local/bin/lpass show {}'.format(host_id)
+            raw_info = subprocess.check_output(lp_show, shell=True)
             username = 'NO USERNAME'
             url = ''
-            if rawInfo.__contains__('Username') and rawInfo.__contains__('URL'):
-                username = rawInfo.split('\n')[1].split('Username:')[1].strip()
-                url = rawInfo.split('\n')[3].split('URL:')[1].strip()
-            wf.add_item(title=iterHost[0],
+            if "Username" in raw_info and "URL" in raw_info:
+                username = raw_info.split('\n')[1].split('Username:')[1].strip()
+                url = raw_info.split('\n')[3].split('URL:')[1].strip()
+            wf.add_item(title=iter_host[0],
                         subtitle=username+'    '+url,
-                        arg=hostId,
+                        arg=host_id,
                         icon='icon.png',
                         valid=True)
 
     wf.send_feedback()
 
-if __name__ == u"__main__":
+if __name__ == "__main__":
     wf = Workflow()
-    sys.exit(wf.run(gethostIds))
+    sys.exit(wf.run(get_host_ids))
